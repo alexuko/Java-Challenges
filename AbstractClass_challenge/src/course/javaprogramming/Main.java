124675634c48ae5178d43fabb5523157484e0628
Alejandro <alextek1885@hotmail.com>
2020-02-27 01:38:22 +0000

    remove item and add item completed

--- AbstractClass_challenge/src/course/javaprogramming/Main.java
+++ AbstractClass_challenge/src/course/javaprogramming/Main.java
@@ -4,5 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        MyLinkedList list = new MyLinkedList(null);
+        list.traverse(list.getRoot());
+
+        String states = "Aguascalientes Zacatecas Baja_California Baja_California_Sur Chihuahua Colima Campeche Coahuila Chiapas Federal_District Durango Guerrero Guanajuato Hidalgo Jalisco México_State Michoacán Morelos Nayarit Nuevo_León Puebla Querétaro Quintana_Roo Sinaloa San_Luis_Potosí Sonora Tabasco Tlaxcala Tamaulipas Veracruz Yucatán Zacatecas";
+        String statesArray[] = states.split(" ");
+        for (String state : statesArray) {
+            list.addItem(new Node(state));
+        }
+
+        list.traverse(list.getRoot());
+        list.removeItem(new Node("Aguascalientes"));
+        list.removeItem(new Node("Zacatecas"));
+        list.removeItem(new Node("Baja_California"));
+        list.removeItem(new Node("null"));
+        list.traverse(list.getRoot());
+
     }
 }
--- AbstractClass_challenge/src/course/javaprogramming/MyLinkedList.java
+++ AbstractClass_challenge/src/course/javaprogramming/MyLinkedList.java
@@ -30,20 +30,17 @@ public class MyLinkedList implements NodeList {
                     currentItem = currentItem.nextItem();
                 }else {
                     //if there is no next, then insert at the end of the list
-                    currentItem.set_next(newItem);
-                    newItem.set_previous(currentItem);
+                    currentItem.set_next(newItem).set_previous(currentItem);
                     return true;
                 }
             }else if(comparison > 0){
                 //newItem is less, insert before
                 if(currentItem.previousItem() != null){
-                    currentItem.previousItem().set_next(newItem);
-                    newItem.set_previous(currentItem.previousItem());
-                    newItem.set_next(currentItem);
-                    currentItem.set_previous(newItem);
+                    currentItem.previousItem().set_next(newItem).set_previous(currentItem.previousItem());
+                    newItem.set_next(currentItem).set_previous(newItem);
                 }else{
                     // the node with a previous is the root
-                    this.root.set_previous(newItem);
+                    newItem.set_next(this.root).set_previous(newItem);
                     this.root = newItem;
                 }
                 return true;
@@ -62,12 +59,54 @@ public class MyLinkedList implements NodeList {
     }
 
     @Override
-    public boolean removeItem(Item item) {
+    public boolean removeItem(Item item) { // item "Colima"
+        if(item == null){
+            System.out.println("Invalid");
+            return false;
+        }
+        Item current = this.root;
+        while (current != null){//while we still having records
+            int comparison = current.compareTo(item);
+            if(comparison == 0){
+                //comparison math the item in the list
+                System.out.println(current.getValue() + " was deleted");
+                if(current == this.root){//if it is the first item of the root
+                    this.root = current.nextItem();
+
+                }else {
+                    // Coahuila -> Colima -> Durango
+                    // Coahuila -> Durango
+                    current.previous.set_next(current.nextItem());
+                    if(current.nextItem() != null){
+                        current.nextItem().set_previous(current.previousItem());
+                    }
+                }
+
+                return true;
+            }else if(comparison < 0){
+                current = current.nextItem();
+            }else { //comparison > 0
+                //item is not in the list
+                return false;
+            }
+
+        }
+        //we reached the end of the list
+        //we have not found the item in the list
         return false;
     }
 
     @Override
     public void traverse(Item root) {
+        if (root == null){
+            System.out.println("List is Empty");
+        }else{
+            while (root != null){
+                System.out.println(root.getValue());
+                root = root.nextItem();
+            }
+
+        }
 
     }
 }
