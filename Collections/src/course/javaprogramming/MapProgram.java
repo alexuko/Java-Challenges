dcd9cc625e282f4fa0ad727c34cc38278f023e83
Alejandro <alextek1885@hotmail.com>
2020-03-29 18:36:25 +0100

    Map add and print methods

---
+++ Collections/src/course/javaprogramming/MapProgram.java
@@ -0,0 +1,24 @@
+package course.javaprogramming;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapProgram {
+    public static void main(String[] args) {
+        Map<Integer,String> lectures = new HashMap<>();
+        lectures.put(1,"Maths");
+        lectures.put(2,"Java");
+        lectures.put(3,"Python");
+        lectures.put(4,"Hacking");
+        lectures.put(4,"Hacking 2");
+
+        System.out.println(lectures.get(4));
+        System.out.println(lectures.containsKey(0));
+
+        System.out.println("========================");
+        for (int key : lectures.keySet()) {
+            System.out.println(key + ": " + lectures.get(key));
+
+        }
+    }
+}
